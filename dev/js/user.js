a92bbe225e4db26e9af908a7e54ce3daaa8adc38
bondarsasha <bondarkarina1991@gmail.com>
2017-07-12 03:44:08 +0300

    my com

---
+++ dev/js/user.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.slider').slick({
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed:2000,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+});
